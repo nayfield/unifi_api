a209e8b4cb71236534ebe098ddbdc632f5715859
rod <rod@thin.nayfield.com>
2020-06-02 23:35:09 -0400

    adding first try

---
+++ try1.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+
+# secrets in the file 'secrets'
+. secrets
+
+# looks like
+#export username=user
+#export password=pass
+#export baseurl=https://hostname:8443
+#export site=default
+
+
+
+. unifi_sh_api
+
+unifi_login
+echo
+
+unifi_api /list/user > userlist.json
+
+echo 'try cat userlist.json | python -m json.tool |less'
+
+# looks like if noted is True in the json record,
+# 'name' is what was in the UI
+# ('hostname' is probably dhcp ask)
+
+# So algorithm to 'move names' ends up like
+# Export:
+# grab json as above from 'old controller'
+# Save json entries 'if noted' to punchlist
+# backup copy of punchlist (totallist)
+# Import: (repeatable)
+# grab userlist from new controller as above
+# load punchlist
+# walk userlist - if mac matches (and does not have name?) 
+#  .  call https://<ip address/hostname>:8443/api/s/<siteid>/upd/user/<_id>
+#     payload:
+#     {name: "device name", note: "device note"}
+#   remove from punchlist
+
